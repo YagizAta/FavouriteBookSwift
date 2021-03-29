a070342432d53ccfbc15a521ba71415048085dd2
YagizAta <yagizata1@hotmail.com>
2021-03-29 21:16:16 +0300

    Initial Commit

---
+++ FavouriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteBook
+//
+//  Created by Yağız Ata Özkan on 29.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
